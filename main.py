e1cb00da4c1b321985d497f1db8efb23376f7517
Pietro Petitti <19675218+BambooFlower@users.noreply.github.com>
2020-06-19 10:40:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Orbit simulation using the pygame library
+# 2-Body problem simulation using the pygame library
 
 
 import pygame
@@ -6,7 +6,6 @@ import math
 
 
 # Initialize the game engine
-pygame.init()
 pygame.font.init()
 
 clock = pygame.time.Clock()
@@ -14,10 +13,10 @@ myfont = pygame.font.SysFont('Calibri', 20)
 target_fps = 60
  
 # Set the height and width of the screen
-SIZE = [1000, 1000]
+SIZE = [1280, 720]
  
 screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Snow Animation")
+pygame.display.set_caption("2-Body Problem Animation")
 
 
 class circle():
@@ -37,7 +36,8 @@ class circle():
         
     def update_pos(self, pos=()):
         if(pos != ()):
-            self.x,self.y = pos
+            self.x = pos
+            self.y = pos
         else:
             self.x += self.x_vel
             self.y += self.y_vel           
@@ -45,88 +45,87 @@ class circle():
     def show(self):
         print("n:{},x:{},y:{}\nvX:{},vY:{}".format(self.num,self.x,self.y,self.x_vel,self.y_vel))
         
-def draw_dot(screen,color,pos,radius,width):
-    pygame.draw.circle(screen,color,pos,radius,width)
+def draw_dot(screen,color,pos,radius):
+    pygame.draw.circle(screen,color,pos,radius)
     
 def g_between(dot1,dot2,fps):
-    r = math.sqrt((dot1.x-dot2.x)**2 + (dot1.y-dot2.y)**2) # Distance between the planet and the sun
+    r = math.sqrt((dot1.x-dot2.x)**2 + (dot1.y-dot2.y)**2) # Distance between the planets
     G = 6.67e-11 # Gravitational constant
-#    print("r:{}".format(r))
+    print("r:{}".format(r))
     
     if(r > 0):
         if(fps == 0):
             fps = target_fps
             
-        # Force and angle between the planets
-        force = (G*dot1.mass*dot2.mass)/r
-        print("F:{}".format(force))
-        
+        # Force and angle between the planets using Newton's Universal Law of Gravitation
+        force = (G*dot1.mass*dot2.mass)/(r**2)
         theta = math.acos(abs(dot1.x-dot2.x)/r)
-        print(theta)
+
     
-        x_vector = force*math.cos(theta)/(fps/60)/dot1.mass
-        y_vector = force*math.sin(theta)/(fps/60)/dot1.mass
+        x_vectorWhite = force*math.cos(theta)/dot1.mass
+        x_vectorYellow = force*math.cos(theta)/dot2.mass
+
+        y_vectorWhite = force*math.sin(theta)/dot1.mass
+        y_vectorYellow = force*math.sin(theta)/dot2.mass
+
         
         if(dot1.x > dot2.x):
-            x_vector = -1 * x_vector
+            x_vectorWhite = -x_vectorWhite
+
         if(dot1.y > dot2.y):
-            y_vector = -1 * y_vector
+            y_vectorWhite = -y_vectorWhite
+
         
-        
-        
-        print("r:{},\ntheta:{}".format(r,x_vector,y_vector,theta))
-        
-        dot1.update_velocity(x_vector,y_vector)
+        dot1.update_velocity(x_vectorWhite,y_vectorWhite)
+        dot2.update_velocity(x_vectorYellow,y_vectorYellow)
         
         # Print the new position
         dot1.show()
-    if((r) <= 2):
-        dot1.update_velocity(0,0,True)
- 
+        dot2.show()
 
 
-# Create one circle
-c1 = circle((450,450),(1.2,0),[255,255,255], mass=1, num=1)
-c2 = circle((500,500),(0,0),[255,255,0], mass=10**10, num=1)
+# Create the two planets
+c1 = circle((600,460), (-0.25, 0), [255,255,255], mass = 100, num = 1)
+c2 = circle((500,400), (0, 0), [255,255,0], mass = 10e10, num = 1)
 
 
 # Loop until the user clicks the close button.
 done = False
 while not done:
     fps = clock.get_fps()
-#    print("fps:{}".format(fps))
+
     for event in pygame.event.get():   # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True   # Flag that we are done so we exit this loop
  
-    # Set the screen background
+    # Set the background and count the fps
     screen.fill([0,0,0])
     
-    # Draw my dot (planet)
-    draw_dot(screen,c1.color,(round(c1.x),round(c1.y)),5,0)    
-    # Draw the focus (sun)
-    draw_dot(screen,c2.color,(round(c2.x),round(c2.y)),10,0)    
+    # Draw the two planets
+    draw_dot(screen, c1.color, (round(c1.x), round(c1.y)), 5)    
+    draw_dot(screen, c2.color, (round(c2.x), round(c2.y)), 10)    
     
-    # Do physics stuff
+    # Compute the force between the planets and update their positions
     g_between(c1,c2,fps)
     c1.update_pos()
+    c2.update_pos()
     
-    # Draw FPS Counter
-    fpssurface = myfont.render(str(round(fps)), False, (255, 255, 255))
-    # Draw X,Y details
-    possurface = myfont.render("The planet position is: " + "  X:"+str(round(c1.x))+"  Y:"+str(round(c1.y)), False, (255, 255, 255))
-    # Draw Forces details
-    forcesruface = myfont.render("The planet velocity is:  " + "  V:"+str(round(math.sqrt(c1.x_vel**2+c1.y_vel**2),2)) + "  Vx:"+str(round(c1.x_vel,2))+"  Vy:"+str(round(c1.y_vel,2)), False, (255, 255, 255))
-    screen.blit(fpssurface,(380,300 + 0))
-    screen.blit(possurface,(300,300 + 20))
-    screen.blit(forcesruface,(300,300 + 40))
+    # Position and velocity details of the two planets
+    infoSurface = myfont.render("Position and Velocity of the Two Planets: ", False, (255, 255, 255))
+    posSurfaceWhite = myfont.render("Position of the White Planet: " + "  X: " + str(round(c1.x)) + "  Y: " + str(round(c1.y)), False, (255, 255, 255))
+    posSurfaceYellow = myfont.render("Position of the Yellow Planet: " + "  X: " + str(round(c2.x)) + "  Y: " + str(round(c2.y)), False, (255, 255, 255))
+
+    forceSurfaceWhite = myfont.render("Velocity of the White Planet:  " + "  V: " + str(round(math.sqrt(c1.x_vel**2 + c1.y_vel**2),2)) + \
+        "  Vx: " + str(round(c1.x_vel,2)) + "  Vy: " + str(round(c1.y_vel,2)), False, (255, 255, 255))
+    forceSurfaceYellow = myfont.render("Velocity of the Yellow Planet:  " + "  V: " + str(round(math.sqrt(c2.x_vel**2 + c2.y_vel**2),2)) + \
+        "  Vx: " + str(round(c2.x_vel,2)) + "  Vy: " + str(round(c2.y_vel,2)), False, (255, 255, 255))
+
+    screen.blit(infoSurface,(0, 2))
+    screen.blit(posSurfaceWhite,(0, 40))
+    screen.blit(posSurfaceYellow,(0, 60))
+    screen.blit(forceSurfaceWhite,(0, 100))
+    screen.blit(forceSurfaceYellow,(0, 120))
     
-    # Go ahead and update the screen with what we've drawn.
+    # Update the screen with what we've drawn.
     pygame.display.flip()
     clock.tick(target_fps)
- 
-# Be IDLE friendly. If you forget this line, the program will 'hang' on exit
-pygame.quit()
-
-del fps
-del target_fps
